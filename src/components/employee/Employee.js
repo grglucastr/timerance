da492a4530b0007af128e17cd53b1dbf173bc286
George Bentes <george.bentes@gmail.com>
2019-05-16 22:15:22 -0400

    Add employee screen styling

--- src/components/employee/Employee.js
+++ src/components/employee/Employee.js
@@ -1,5 +1,6 @@
 import React from 'react'
-import {View, Text} from 'react-native'
+import {View, Text, StyleSheet} from 'react-native'
+import { TextInput, TouchableOpacity } from 'react-native-gesture-handler';
 
 class Employee extends React.Component{
 
@@ -9,11 +10,56 @@ class Employee extends React.Component{
 
   render(){
     return(
-      <View>
-        <Text>Emplasdfoyee Insert Screen</Text>
+      <View style={styles.container}>
+        
+        <Text style={styles.title}>Timerance</Text>
+
+        <View style={styles.controlContainer} >
+          <TextInput 
+            placeholder={`Enter employee's ID number`}
+            style={styles.input} />
+        
+          <TouchableOpacity 
+            style={styles.button}
+            onPressOut={()=>this.props.navigation.navigate('Detail')}>
+            <Text style={styles.buttonText} >Proceed</Text>
+          </TouchableOpacity>
+        </View>
       </View>
     )
   }
 }
+
+const styles = StyleSheet.create({
+  container: {
+    alignItems: 'center',
+  },
+  title:{
+    fontSize: 25,
+    marginTop: '20%',
+  },
+  controlContainer:{
+    width:'100%',
+    marginTop: '40%',
+  },
+  input: {
+    borderBottomColor: '#ccc',
+    borderBottomWidth: 1,
+    width:'90%',
+    alignSelf: 'center',
+  },
+  button: {
+    backgroundColor: '#ccc',
+    borderRadius:3,
+    width:'60%',
+    padding: 10,
+    marginTop:25,
+    alignSelf: 'center',
+  },
+  buttonText:{
+    color: '#000',
+    textAlign: 'center',
+  },
+});
 
 export default Employee
