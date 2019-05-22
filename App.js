d2bf6002dfb5fdfe5fc0a0bac828b786a7d03353
George Bentes <george.bentes@gmail.com>
2019-05-21 21:20:13 -0400

    Remove uncessary imports

--- App.js
+++ App.js
@@ -1,8 +1,5 @@
 import React from 'react';
-import {StyleSheet, Text, View} from 'react-native';
-
 import Home from './src/components/home';
-import Employee from './src/components/employee';
 
 export default class App extends React.Component {
   render() {
